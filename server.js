ab3bfba683002c7e3dd4561265a6466ab1f45277
Jay <03jaypaddle@gmail.com>
2023-08-13 13:30:12 -0400

    DataBase Added

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 // npm installs
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser');
 
 const app = express()
-app.listen(3000)
+
+// database connection
+const dbURI = 'mongodb+srv://jrp329:Awesome101@rgm.bciueqx.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true,})
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
+
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
+
 
 // view engine
 app.set('view engine', 'ejs');
