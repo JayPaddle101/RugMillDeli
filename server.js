c0803ad1f2126748facf6abae62af8ad9ca01cc3
Jay <03jaypaddle@gmail.com>
2023-08-13 21:36:12 -0400

    Added Login/Sign Up  FrontEnd

--- server.js
+++ server.js
@@ -26,3 +26,11 @@ app.get('/', function (req, res) {
   res.render("index")
 })
 
+app.get('/login', function (req, res) {
+  res.render("login")
+})
+
+app.get('/signup', function (req, res) {
+  res.render("signup")
+})
+
